86418e3b7ffa8cabaf1eef415214aae21440a089
Erica Jiang <115647009+yuchenie@users.noreply.github.com>
2025-12-03 02:05:35 -0500

    integrating halls

--- BLDC-Motor/BLDC-Motor.c
+++ BLDC-Motor/BLDC-Motor.c
@@ -12,19 +12,25 @@
 #include "gate_drive.pio.h"
 
 #define OUT_PINS 10
+#define PWM_PIN 25
 
 volatile int state = 0;
+uint32_t prev_data = 0b000000; 
 
 struct repeating_timer timer;
 
 const uint8_t shift[6][3] = {
-    {2,1,0},
-    {4,3,2},
-    {4,1,0},
-    {0,5,4},
-    {2,5,4},
-    {0,3,2},
+    {3,0,1},
+    {5,2,3},
+    {5,0,1},
+    {1,4,5},
+    {3,4,5},
+    {1,2,3},
 };
+
+// TODO adjust wiring onto pico because it's wrong
+#define NUM_INPUTS 3
+const uint input_pins[NUM_INPUTS] = {16, 17, 18};
 
 // Write `period` to the input shift register
 void pio_pwm_set_period(PIO pio, uint sm, uint32_t period) {
@@ -40,13 +46,11 @@ void pio_pwm_set_level(PIO pio, uint sm, uint32_t level) {
     pio_sm_put_blocking(pio, sm, level);
 }
 
-bool timer_callback(struct repeating_timer *t)
-{
-    state += 1;
-    state = state % 6;
-    cancel_repeating_timer(&timer);
-    add_repeating_timer_ms(100, (repeating_timer_callback_t)timer_callback, NULL, &timer);
-    return true;
+void irq_handler(uint gpio, uint32_t events) {
+    int a = gpio_get(input_pins[0]);
+    int b = gpio_get(input_pins[1]);
+    int c = gpio_get(input_pins[2]);
+    state = (a << 2) | (b << 1) | (c);
 }
 
 int main() {
@@ -64,10 +68,10 @@ int main() {
     PIO pwm_pio = pio0;
     int pwm_sm = 0;
     uint pwm_offset = pio_add_program(pwm_pio, &pwm_program);
-    pwm_program_init(pwm_pio, pwm_sm, pwm_offset, PICO_DEFAULT_LED_PIN);
+    pwm_program_init(pwm_pio, pwm_sm, pwm_offset, PWM_PIN);
 
     pio_pwm_set_period(pwm_pio, pwm_sm, 255);
-    pio_pwm_set_level(pwm_pio, pwm_sm, 128);
+    pio_pwm_set_level(pwm_pio, pwm_sm, 100);
 
     // GATE DRIVE PIO state machine
     PIO gd_pio = pio1;
@@ -76,11 +80,32 @@ int main() {
     gate_drive_program_init(gd_pio, gd_sm, gd_offset, OUT_PINS);
     pio_sm_set_enabled(gd_pio, gd_sm, true);
 
-    add_repeating_timer_ms(100, (repeating_timer_callback_t)timer_callback, NULL, &timer);
+    // hall effect sensors
+    // gpio_set_irq_enabled_with_callback(input_pins[0], GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &irq_handler);
+    for (int i = 0; i < NUM_INPUTS; i++)
+    {
+        gpio_init(input_pins[i]);
+        gpio_set_dir(input_pins[i], GPIO_IN);
+        gpio_pull_up(input_pins[i]);
+        gpio_set_irq_enabled(input_pins[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    }
 
     while (true) {
-        int pwm = gpio_get(PICO_DEFAULT_LED_PIN);
+        int a = gpio_get(input_pins[0]);
+        int b = gpio_get(input_pins[1]);
+        int c = gpio_get(input_pins[2]);
+        state = ((a << 2) | (b << 1) | (c)) - 1;
+
+        printf("data: %d\n", state);
+
+        int pwm = gpio_get(PWM_PIN);
         uint32_t data = 0b000000 | (1 << shift[state][0]) | (pwm << shift[state][1]) | (!pwm << shift[state][2]);
-        pio_sm_put_blocking(gd_pio, gd_sm, data);
+        
+        if (prev_data != data) { 
+            uint32_t test = 0b000000 | (1 << shift[state][0]);
+            pio_sm_put_blocking(gd_pio, gd_sm, test);
+            pio_sm_put_blocking(gd_pio, gd_sm, data);
+            prev_data = data;
+        }        
     }
 }
