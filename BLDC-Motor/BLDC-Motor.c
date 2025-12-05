85ac0dc80c5a4da971fbaea553f0bfaacee2106d
Erica Jiang <115647009+yuchenie@users.noreply.github.com>
2025-12-05 17:29:18 -0500

    fix zero velocity issue

--- BLDC-Motor/BLDC-Motor.c
+++ BLDC-Motor/BLDC-Motor.c
@@ -1,18 +1,11 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include <stdio.h>
-#include "hardware/irq.h"
-#include "hardware/gpio.h"
-#include "hardware/clocks.h"
+#include <string.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
+#include "hardware/adc.h"
 #include "pwm.pio.h"
 #include "gate_drive.pio.h"
-
+#include "pt_cornell_rp2040_v1_4.h"
 
 #define OUT_PINS 10
 #define PWM_PIN 25
@@ -20,8 +13,26 @@
 volatile int state = 0;
 uint32_t prev_data = 0b000000; 
 
+#define TAU 1e6 // time constant in us for low-pass filter
+
+volatile float motor_rpm = 0.0f;
+const float RATED_MOTOR_RPM = 3000.0f;
+const float RATED_MOTOR_VOLTAGE = 12.0f;
+const float MAX_VOLTAGE_AT_STALL = 6.0f;
+#define MIN_RPM 25.0f
+#define WRAPVAL 255
+
+#define THROTTLE_ADC 26
+
+// if the motor is stationary, then the interrupt needs to be called
+// periodically to avoid the interrupt from never being called
+// and the motor_rpm from never being updated
 struct repeating_timer timer;
 
+// time of last irq
+volatile uint32_t irq_prev_time = 0;
+
+// commutation table
 const uint8_t shift[6][3] = {
     {3,0,1},
     {5,2,3},
@@ -38,8 +49,6 @@ int pwm_sm = 0;
 // GATE DRIVE PIO state machine
 PIO gd_pio = pio1;
 int gd_sm = 1;
-
-volatile int pwm = 0;
 
 // TODO adjust wiring onto pico because it's wrong
 #define NUM_INPUTS 3
@@ -60,10 +69,35 @@ void pio_pwm_set_level(PIO pio, uint sm, uint32_t level) {
 }
 
 void update_control() {
+    int pwm = gpio_get(PWM_PIN);
     uint32_t data = 0b000000 | (1 << shift[state][0]) | (pwm << shift[state][1]) | (!pwm << shift[state][2]);    
     uint32_t test = 0b000000 | (1 << shift[state][0]);
     pio_sm_put_blocking(gd_pio, gd_sm, test);
     pio_sm_put_blocking(gd_pio, gd_sm, data);
+}
+
+bool timer_callback(struct repeating_timer *t)
+{
+    // no velocity on boot
+    if (irq_prev_time == 0) {
+        motor_rpm = 0.0f;
+    }
+
+    int timer_current_time = time_us_64();
+    float timer_period = (float)(timer_current_time - irq_prev_time);
+    
+    // low-pass filter
+    float raw_rpm = 2.5e6f / timer_period; 
+    float alpha = timer_period / (TAU + timer_period);
+    motor_rpm = alpha * raw_rpm + (1.0f - alpha) * motor_rpm;
+    
+    // minimum measurable rpm is 25 rpm
+    if (motor_rpm < MIN_RPM)
+        motor_rpm = 0.0f;
+    
+    cancel_repeating_timer(&timer);
+    add_repeating_timer_ms(100, (repeating_timer_callback_t)timer_callback, NULL, &timer);
+    return true;
 }
 
 void irq_handler(uint gpio, uint32_t events) {
@@ -72,21 +106,75 @@ void irq_handler(uint gpio, uint32_t events) {
     int c = gpio_get(input_pins[2]);
     state = ((a << 2) | (b << 1) | (c)) - 1;
     update_control();
+
+    int irq_current_time = time_us_64();
+
+    // time between steps in microseconds
+    float step_period = (float)(irq_current_time - irq_prev_time);
+    if (step_period <= 800.0f) { // invalid period, ignore
+        return;
+    }
+
+    irq_prev_time = irq_current_time;
+
+    // step/us * 1 elec. rev/6 steps * 1 mech. rev/4 elec. rev * 1e6 us/s * 60 s/min
+    // = 2.5e6 rpm
+    float raw_rpm = 2.5e6f / step_period; 
+
+    // low-pass filter
+    float alpha = step_period / (TAU + step_period);
+    motor_rpm = alpha * raw_rpm + (1.0f - alpha) * motor_rpm;
+
+    // reset the timer to call this function again in 100ms if no step is detected
+    cancel_repeating_timer(&timer);
+    add_repeating_timer_ms(100, (repeating_timer_callback_t)timer_callback, NULL, &timer);
 }
 
 void pwm_irq0() {
     pio_interrupt_clear(pwm_pio, 0);
-    pwm = 0;
     update_control();
 }
 
-void pwm_irq1() {
-    pio_interrupt_clear(pwm_pio, 1);
-    pwm = 1;
-    update_control();
+int duty_cycle_to_level(float duty_cycle)
+{
+    if (duty_cycle < 0.0f)
+        duty_cycle = 0.0f;
+    if (duty_cycle >= 1.0f) {
+        duty_cycle = 1.0f;
+    }
+
+    return (int)(duty_cycle * WRAPVAL);
 }
 
-void initialize() {
+int adc_deadzone(int adc_value)
+{
+    const int DEADZONE = 100; // adc units
+
+    if (adc_value < DEADZONE)
+        return 0;
+    if (adc_value > 4095 - DEADZONE)
+        return 4095;
+    adc_value = (adc_value - DEADZONE) * 4095 / (4095 - DEADZONE);
+    return adc_value;
+}
+
+// User input thread. User can change throttle.
+static PT_THREAD (user_input(struct pt *pt))
+{
+    PT_BEGIN(pt) ;
+    while(1) {
+        float throttle = (float)adc_deadzone(adc_read()) / 4095.0f;
+        float duty = throttle * (motor_rpm / RATED_MOTOR_RPM + MAX_VOLTAGE_AT_STALL / RATED_MOTOR_VOLTAGE);
+        pio_pwm_set_level(pwm_pio, pwm_sm, duty_cycle_to_level(duty));
+
+        // printf("Velocity: %f\tThrottle: %f\tDuty: %d\n", motor_rpm, throttle, duty_cycle_to_level(duty));
+
+        PT_YIELD_usec(30000) ;
+    }
+    PT_END(pt) ;
+}
+
+int main() {
     stdio_init_all();
 
     // PWM PIO state machine
@@ -96,13 +184,9 @@ void initialize() {
     irq_set_exclusive_handler(PIO0_IRQ_0, pwm_irq0);
     irq_set_enabled(PIO0_IRQ_0, true);
     pio_set_irq0_source_enabled(pwm_pio, pis_interrupt0, true);
-    
-    irq_set_exclusive_handler(PIO0_IRQ_1, pwm_irq1);
-    irq_set_enabled(PIO0_IRQ_1, true);
-    pio_set_irq1_source_enabled(pwm_pio, pis_interrupt1, true);
 
-    pio_pwm_set_period(pwm_pio, pwm_sm, 255);
-    pio_pwm_set_level(pwm_pio, pwm_sm, 50);
+    pio_pwm_set_period(pwm_pio, pwm_sm, WRAPVAL);
+    pio_pwm_set_level(pwm_pio, pwm_sm, 0);
 
     // GATE DRIVE PIO state machine
     uint gd_offset = pio_add_program(gd_pio, &gate_drive_program);
@@ -118,11 +202,17 @@ void initialize() {
         gpio_pull_up(input_pins[i]);
         gpio_set_irq_enabled(input_pins[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
     }
-}
 
-int main() {
-    initialize();
+    // add a timer that will call the irq_handler if no step has been detected for 100ms
+    add_repeating_timer_ms(100, (repeating_timer_callback_t)timer_callback, NULL, &timer);
 
-    while (true) {
-    }
+    // configure adc for throttle input
+    gpio_init(THROTTLE_ADC);
+    gpio_set_dir(THROTTLE_ADC, GPIO_IN);
+    adc_init();
+    adc_gpio_init(THROTTLE_ADC);
+    adc_select_input(0);
+    
+    pt_add_thread(user_input);
+    pt_schedule_start ;
 }
